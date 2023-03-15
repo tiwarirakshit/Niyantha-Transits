848581fae4c91ede1aab11d5dc79ae6f49f62ef9
Rakshit Tiwari <rakshittiwari02@gmail.com>
2023-03-16 00:26:23 +0530

    Merge Issues Resolved

--- screens/CartScreen.js
+++
@@ -1,7 +0,0 @@
-import { Text } from "react-native";
-
-function CartScreen() {
-    return <Text>Cart screen</Text>
-}
-
-export default CartScreen;
--- screens/HomeScreen.js
+++
@@ -1,7 +0,0 @@
-import { Text } from "react-native";
-
-function HomeScreen() {
-    return <Text>home</Text>
-}
-
-export default HomeScreen;
